be92c61763dc800588ac081b6ae9cad0c3149f36
grovesdixon <grovesdixon@gmail.com>
2014-10-22 17:05:33 -0500

    Add a percent identity filter to get_reciprocal_orthos.py so that sequences are only stored as reciprocals if they had greater than a given hit percentage (the number of identical bases divided by the hit length).
    Add filtering option to mergeReciprocalOrthos.py so that it will only output
    orthologs for which more than a given cutoff of species have representative
    
    Coupling these two new filtering options lets you select only those orthologs that
    seem to be nearly complete genes and found most or all of the species considered.

--- get_reciprocal_orthos.py
+++ get_reciprocal_orthos.py
@@ -2,7 +2,7 @@
 ##get_reciprocal_orthos.py.py
 ##written 9/16/14 by Groves Dixon
 ProgramName = 'get_reciprocal_orthos.py'
-LastUpdated = '9/16/14'
+LastUpdated = '10/21/14'
 By = 'Groves Dixon'
 VersionNumber = '1.0'
 print "\nRunning Program {}...".format(ProgramName)
@@ -11,14 +11,21 @@ VersionString = '{} version {} Last Updated {} by {}'.format(ProgramName, Versio
 
 Description = '''
 Description:
-This script pulls a set of reciprocal orthologs based on 
-a pair of reciprocal blast outputs and the fasta files used as queries.
+This script pulls a set of reciprocal best hits based on 
+a pair of reciprocal Blast outputs and the fasta files used as queries.
+It has options to filter how the reciprocal blasts are selected.
 
 '''
 
 AdditionalProgramInfo = '''
 Additional Program Information:
 
+To use this script, first get two fasta files
+that you think contain orthologs.
+Blast each fasta against the other using out format 5 
+so that biopython can parse the blast output file.
+Now you have the two fasta files, and two blast output
+files to pass as arguments to this script.
 
 '''
 
@@ -38,13 +45,14 @@ Start_time = time.time() ##keeps track of how long the script takes to run
 ##Set Up Argument Parsing
 parser = argparse.ArgumentParser(description=Description, epilog=AdditionalProgramInfo) ##create argument parser that will automatically return help texts from global variables above
 parser.add_argument('-br1', required = True, dest = 'br1', help = 'The first blast output file')
-parser.add_argument('-br2', required = False, dest = 'br2', help = 'The second (reciprocal to first) blast output file')
+parser.add_argument('-br2', required = True, dest = 'br2', help = 'The second (reciprocal to first) blast output file.')
 parser.add_argument('-fa1', required = True, dest = 'fa1', help = 'The fasta file that was the query for the first blast (and the database for the second blast)')
 parser.add_argument('-fa2', required = True, dest = 'fa2', help = 'The fasta file that was the query for the second blast (and the database for the first blast)')
-parser.add_argument('-e', required = False, dest = 'eval', help = 'The theshold to use for e-value. (Default is 1e-20)')
-parser.add_argument('-o', required = True, dest = 'out', default = 1e-20, help = 'The desired name for the output file')
-parser.add_argument('-id1', required = False, dest = 'id1', default = 0, help = 'The Position of the Identifying Information in the Sequence Names for fa1 (split by blank space). Default = 0')
-parser.add_argument('-id2', required = False, dest = 'id2', default = 0, help = 'The Position of the Identifying Information in the Sequence Names for fa2 (split by blank space). Default = 0')
+parser.add_argument('-o', required = True, dest = 'out', help = 'The desired name for the output file')
+parser.add_argument('-id1', required = False, dest = 'id1', default = 0, type = int, help = 'The Position of the Identifying Information in the Sequence Names for fa1 (split by blank space). Default = 0')
+parser.add_argument('-id2', required = False, dest = 'id2', default = 0, type = int, help = 'The Position of the Identifying Information in the Sequence Names for fa2 (split by blank space). Default = 0')
+parser.add_argument('-e', required = False, dest = 'eval', default = 1e-20, help = 'Set an optional theshold to use for e-value. (Default is 1e-20)')
+parser.add_argument('-hp', required = False, dest = 'hitPct', default = 80, type = float, help = 'Set an optional threshold for hit percentage. (Default 80)')
 args = parser.parse_args()
 
 #Assign Arguments
@@ -57,8 +65,9 @@ Ecut = args.eval
 Ecut = float(Ecut)
 ID1 = args.id1 ##the location of the identifying information in the seq names from fa1 when split by blank space
 ID2 = args.id2 ##the location of the identifying information in seq names from fa2 when split by blank space
-ID1 = int(ID1)
-ID2 = int(ID2)
+# ID1 = int(ID1)
+# ID2 = int(ID2)
+HitPctCut = args.hitPct / 100.0
 NA = 'none'
 
 
@@ -93,6 +102,21 @@ def build_bestHit_dict(br, entryList, Id):
             # print "entry {}     hitTitle {}".format(entry, hitName)
             hitName = hitName.split()[1].split()[Id]
             # print "entry {}     hitName {}".format(entry, hitName)
+            #check that the hit percent is above cutoff, if it isn't then change hitName to 'Pct_Over'
+            # if topHit.hsps[0].
+            # print
+            # print
+            # print topHit.hsps[0].match
+            # print topHit.hsps[0].expect
+            # print topHit.length
+            # print topHit.hsps[0].identities
+            
+            #check that the hit percentage is higher than the cutoff
+            hitPct = float(topHit.hsps[0].identities)/float(topHit.length)
+            # print hitPct
+            if hitPct < HitPctCut:
+                # print 'failed hit percent'
+                hitName = 'Hit_Pct_to_low'
             #look at the evalue for the top hit
             if topHit.hsps[0].expect > Ecut:
                 hitName = 'Eval_Over'
@@ -112,12 +136,17 @@ def pull_reciprocals(fa1List, fa2List, fa1Hits, fa2Hits):
     missDict = {}
     resultDict = {}
     eFailList = []
+    hpFailList = []
     noHitList = []
     for i in fa1List:
         bestHit = fa1Hits[i]
         #if the best hit was above the evalue threshold, store as an Efail and continue
         if bestHit == 'Eval_Over':
             eFailList.append(i) #note the evalue fail
+            resultDict[i] = NA #record NA for the result
+            continue
+        if bestHit == 'Hit_Pct_to_low':
+            hpFailList.append(i) #note the hit percentage fail
             resultDict[i] = NA #record NA for the result
             continue
         #check the reciprocal hit
@@ -135,14 +164,17 @@ def pull_reciprocals(fa1List, fa2List, fa1Hits, fa2Hits):
     recips = len(recipDict.keys())
     misses = len(missDict.keys())
     eFails = len(eFailList)
+    hpFails = len(hpFailList)
     noHits = len(noHitList)
-    sumTot = recips + misses + eFails + noHits
+    sumTot = recips + misses + eFails + noHits + hpFails
     totOutput = len(resultDict.keys())
     print "\n{} Total Entries Found in {}".format(total, FA1)
     print "\nEvalue Cutoff was {}".format(Ecut)
+    print "\nHit Percentage Cutoff was {}".format(HitPctCut)
     print "\n{} Entries had Reciprocal Best Hits with {}. Percent = {}%".format(recips, FA2, float(recips)/float(total))
     print "\n{} Entries had Hits, but Were not Reciprocal. Percent = {}%".format(misses, float(misses)/float(total))
     print "\n{} Entries hit at the original Evalue cutoff (used in the blast), but failed with the theshold given here".format(eFails)
+    print "\n{} Entires hit but did not have high enough hit percentages and were not recorded".format(hpFails)
     print "\n{} Entries Never Hit".format(noHits)
     print "\nTotal Entires = {}; Total Accounted For Here = {}; Total to Output = {}".format(total, sumTot, totOutput)
     if total == sumTot == totOutput:
